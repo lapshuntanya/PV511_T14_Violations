5262e963fb04d51f194af89a0c3ae294d35fb536
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-03-17 19:53:33 +0200

    Driver

--- Driver.h
+++ Driver.h
@@ -9,5 +9,28 @@ struct Driver {
     int sizeViol = 0;
     Violation* arrViol = nullptr;
 
+    void fillDriver() {
+        cout << "Input name:";
+        cin.getline(name, 100);
+        cout << "Input number:";
+        cin >> carNumber;
+        sizeViol = 1;
+        arrViol = new Violation[1];
+        arrViol[0].fillViolation();
+    }
 
+    void printDriver() {
+        cout << "Driver: " << name << endl;
+        cout << "Car:    " << carNumber << endl;
+        for (int i = 0; i < sizeViol; i++) {
+            arrViol[i].printTableViolation();
+        }
+        cout << "=============================================\n\n";
+    }
+
+    void addViol() {
+        Violation tmp;
+        tmp.fillViolation();
+        addItemBack(arrViol, sizeViol, tmp);
+    }
 };
